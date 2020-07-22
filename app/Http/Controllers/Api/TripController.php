6b72c00bb3aa35b5b4a0859063e9bc7522c7128b
SiThu182 <sithuaung182@gmail.com>
2020-07-22 14:26:14 +0800

    update search

--- app/Http/Controllers/Api/TripController.php
+++ app/Http/Controllers/Api/TripController.php
@@ -98,22 +98,19 @@ class TripController extends Controller
      public function searchTrip(Request $request)
     {
         $departure_time = request('departure_time');
-      
+        
         $route = request('route');
+        // echo $departure_time;
+        // echo $route;
         $trips = DB::table('trips')
                 ->select('trips.*')
-                ->where('trips.departure_time','>','%'.$departure_time.'%')
+                ->where('trips.departure_time','like','%'.$departure_time.'%','and','trips.permission','=','1')
                 ->where('trips.route_id', '=', $route)
                 ->get();
-            if ( count($trips) > 0) {
+         
                 return response()->json([
             'trips' => $trips 
-             ]);   
-            }else{
-                 return response()->json([
-            'trips' => " No ticket available on the selected date. Please try searching for other dates."
-             ]);   
-            }
+             ]);
            
        
      
